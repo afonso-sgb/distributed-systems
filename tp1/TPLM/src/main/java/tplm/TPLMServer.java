59ea3a0d242959f214eb59291aceb687dc00ddcb
Pedro Mateus <mateus.pmc2000@gmail.com>
2025-04-26 23:32:00 +0100

    Parametros de input melhorados no TPLM

--- tp1/TPLM/src/main/java/tplm/TPLMServer.java
+++ tp1/TPLM/src/main/java/tplm/TPLMServer.java
@@ -1,29 +1,32 @@
 package tplm;
 
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 
 import java.net.InetSocketAddress;
 
 public class TPLMServer {
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.out.println("Usage: <ZK_HOST> <PORT>");
+        if (args.length != 3) {
+            // System.out.println("Usage: <ZK_HOST> <PORT>");
+            System.out.println("Usage: <zkIP> <zkPort-2181> <TPLMport>");
             return;
         }
 
-        String zkHost = args[0];
-        int port = Integer.parseInt(args[1]);
+        String zkIP = args[0];
+        int zkPort = Integer.parseInt(args[1]);
+        String zkHost = zkIP + ":" + zkPort;
+
+        int TPLMport = Integer.parseInt(args[2]);
 
         Server server = NettyServerBuilder
-                .forAddress(new InetSocketAddress(zkHost, port))
+                .forAddress(new InetSocketAddress(zkIP, TPLMport))
                 //.forPort(port)
                 .addService(new TPLMServiceImpl(zkHost))
                 .build();
 
         server.start();
-        System.out.println("TPLM is running on port " + port);
+        System.out.println("TPLM is running on port " + TPLMport);
         server.awaitTermination();
     }
 }
